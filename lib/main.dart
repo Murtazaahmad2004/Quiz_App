0ae36463a1312699edc6572f701951a6ca331b38
Murtaza Ahmad <shmurtazaahmad336@gmail.com>
2024-09-14 23:11:12 +0500

    This Is my First Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: QuizScreen(),
     );
   }
